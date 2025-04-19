28b753dc9aa02de9674659ca1fdbd21c11493437
HongLinh719 <honglinh719@gmail.com>
2025-04-19 20:28:14 +0700

    Sửa đường dẫn cho GitHub Pages

--- js/config.js
+++ js/config.js
@@ -8,9 +8,24 @@ export const CONFIG = {
         5: { requiredPoints: 100, title: "Siêu sao" }
     },
     AUDIO_PATHS: {
-        goodTask: "./audio/congratulation.mp3",
-        levelUp: "./audio/level_up.mp3",
-        badTask: "./audio/chua_tot.mp3",
-        firework: "./audio/firework.mp3"
+        goodTask: "/thoigianbieunew/audio/congratulation.mp3",
+        levelUp: "/thoigianbieunew/audio/level_up.mp3",
+        badTask: "/thoigianbieunew/audio/chua_tot.mp3",
+        firework: "/thoigianbieunew/audio/firework.mp3"
+    },
+    IMAGE_PATHS: {
+        tree: {
+            day1: "/thoigianbieunew/images/tree_day1.png",
+            day2: "/thoigianbieunew/images/tree_day2.png",
+            day3: "/thoigianbieunew/images/tree_day3.png",
+            day4: "/thoigianbieunew/images/tree_day4.png",
+            day5: "/thoigianbieunew/images/tree_day5.png",
+            day6: "/thoigianbieunew/images/tree_day6.png",
+            day7: "/thoigianbieunew/images/tree_day7.png",
+            full: "/thoigianbieunew/images/tree_full.png"
+        },
+        water: "/thoigianbieunew/images/water.gif",
+        flower: "/thoigianbieunew/images/flower.png",
+        gift: "/thoigianbieunew/images/gift.png"
     }
 };
